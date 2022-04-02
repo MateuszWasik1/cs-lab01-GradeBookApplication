a6d0874acdcac202bc86535e0112129521bec364
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2022-04-02 17:35:16 +0200

    Fix 1 - 6 Test Case

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public GradeBookType Type { get; set; }
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
