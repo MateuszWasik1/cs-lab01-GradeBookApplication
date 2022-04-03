a5902eb17422ae9e3451cb773df412755fffa10b
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2022-04-03 15:56:04 +0200

    Fix 7 - 12 Test Case

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -12,6 +13,26 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        //public override char GetLetterGrade(double averageGrade)
+        //{
+        //    if (Students.Count() < 5)
+        //       throw new InvalidOperationException();
+        //    return base.GetLetterGrade(averageGrade);
+        //}
+        public override void CalculateStatistics()
+        {
+            if (Students.Count() < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 }
